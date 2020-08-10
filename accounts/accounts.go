7134c9576a7705438433de80aea7083a65b247f0
peakhhok <69235026+peakhhok@users.noreply.github.com>
2020-08-10 18:01:43 +0900

    nomad banking practice

---
+++ accounts/accounts.go
@@ -0,0 +1,15 @@
+package accounts
+
+//Account struct
+type Account struct {
+	owner   string
+	balance int
+}
+
+//NewAccount create account
+func NewAccount(owner string) *Account {
+
+	account := Account{owner: owner, balance: 0}
+	return &account
+
+}
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"GoPractice/accounts"
+	"fmt"
+)
+
+func main() {
+
+	account := accounts.NewAccount("HO")
+	fmt.Println(account)
+
+}
