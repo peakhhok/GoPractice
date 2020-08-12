d609f0419828c201ded705eb54193fb6621207ca
peakhhok <69235026+peakhhok@users.noreply.github.com>
2020-08-12 17:40:07 +0900

    banking system practice
    by NomadCoder

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -1,10 +1,17 @@
 package accounts
 
+import (
+	"errors"
+	"fmt"
+)
+
 //Account struct
 type Account struct {
 	owner   string
 	balance int
 }
+
+var errNoMoney = errors.New("Can't withdraw")
 
 //NewAccount create
 func NewAccount(owner string) *Account {
@@ -12,6 +19,40 @@ func NewAccount(owner string) *Account {
 	return &account
 
 }
-func (a Account) Deposit(amount int) {
 
+//Deposit x amount on your account
+func (a *Account) Deposit(amount int) {
+	a.balance += amount
+
+}
+
+// Balance of your account
+func (a Account) Balance() int {
+	return a.balance
+}
+
+//Withdraw x amount from amount
+func (a *Account) Withdraw(amount int) error {
+	if a.balance < amount {
+		return errNoMoney
+	}
+	a.balance -= amount
+	return nil
+
+}
+
+//ChangeOwner new account
+func (a *Account) ChangeOwner(newOwner string) {
+	a.owner = newOwner
+
+}
+
+//Owner introduce
+func (a Account) Owner() string {
+
+	return a.owner
+}
+
+func (a Account) String() string {
+	return fmt.Sprint(a.Owner(), "'s account.\nHas: ", a.Balance())
 }
