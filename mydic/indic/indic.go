27c3fd03218d42c7099999a43130aa17b4754630
peakhhok <69235026+peakhhok@users.noreply.github.com>
2020-08-14 18:07:44 +0900

    Dictionary complete
    done

--- mydic/indic/indic.go
+++ mydic/indic/indic.go
@@ -5,8 +5,11 @@ import "errors"
 //Dictionary type
 type Dictionary map[string]string
 
-var errNotFound = errors.New("Not Found")
-var errWordExists = errors.New("That word already exists")
+var (
+	errNotFound   = errors.New("Not Found")
+	errWordExists = errors.New("That word already exists")
+	errCantUpdate = errors.New("You can't Update")
+)
 
 //Search word
 func (d Dictionary) Search(word string) (string, error) {
@@ -29,5 +32,33 @@ func (d Dictionary) Add(word, def string) error {
 		return errWordExists
 	}
 
+	return nil
+}
+
+//Update text
+func (d Dictionary) Update(word, definition string) error {
+
+	_, err := d.Search(word)
+	switch err {
+	case nil:
+		d[word] = definition
+	case errNotFound:
+		return errCantUpdate
+	}
+	return nil
+
+}
+
+//Delete Word
+func (d Dictionary) Delete(word string) error {
+
+	_, err := d.Search(word)
+	switch err {
+	case nil:
+		delete(d, word)
+	case errWordExists:
+		return errNotFound
+
+	}
 	return nil
 }
--- mydic/main.go
+++ mydic/main.go
@@ -8,17 +8,13 @@ import (
 
 func main() {
 	dictionary := indic.Dictionary{}
-	word := "hi"
-	definition := "say hi to you"
-	err := dictionary.Add(word, definition)
+	baseword := "hi"
+	dictionary.Add(baseword, "First")
+	dictionary.Search(baseword)
+	dictionary.Delete(baseword)
+	word, err := dictionary.Search(baseword)
 	if err != nil {
 		fmt.Println(err)
 	}
-	hello, _ := dictionary.Search(word)
-	fmt.Println(hello)
-	err2 := dictionary.Add(word, definition)
-	if err2 != nil {
-		fmt.Println(err2)
-	}
-
+	fmt.Println(word)
 }
