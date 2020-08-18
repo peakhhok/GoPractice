cdebda00ed998cf2a087a5d7b0c0c6245591d4b2
peakhhok <69235026+peakhhok@users.noreply.github.com>
2020-08-18 16:58:55 +0900

    help sadari

--- sadari/main.go
+++ sadari/main.go
@@ -1,20 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
-	fmt.Println("몇명임?: ")
+	fmt.Print("몇명임?: ")
 	var a int
 	fmt.Scanln(&a)
 	member := make([]string, a)
 	for b := 0; b < a; b++ {
-		fmt.Println("이름입력_")
-		fmt.Scanln(member[b])
+		fmt.Println("이름입력", b, ":")
+
+		fmt.Scan(member[b])
 	}
 	fmt.Print("지금 멤버는: ")
-	fmt.Print(member)
+	fmt.Println(member)
+	time.Sleep(time.Second * 2)
 	if len(member)%2 == 0 {
 		fmt.Println("팀을 나눕니다.")
+		rand.Seed(time.Now().UnixNano())
+		rand.Shuffle(len(member), func(i, j int) { member[i], member[j] = member[j], member[i] })
+		fmt.Println(member)
 
 	}
 
